b0344c5d46f04beed6e4202cdd75947eadf4ecc9
unknown <kmckendall@gmail.com>
2017-11-08 00:37:11 -0500

    updated the program to choose the wor and split the word into letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var hpTerms = ["Harry Potter", " Ron Weasley", "Hermione Granger", "Draco Malfoy
     "Neville Longbottom", "Fred and George Weasley", "Unicorn", "Goblin", "Centaurs", "Ghosts", "Transfiguration",
     "Potions", "Herbology", "Defense Against The Dark Arts", "History of Magic", "Slytherin", "Gryffindor",
     "Hufflepuff", "Ravenclaw", "Sorcerer's Stone", "Chamber of Sercrets", "Prisoner of Azkaban", "Goblet of Fire",
-    "Order of the Phoenix", "Half-Blood Prince", "Deathly Hallows", "Ginny Weasley", "Mr. Arthur Weasley", "Mrs. Molly Weasley",
+    "Order of the Phoenix", "Half Blood Prince", "Deathly Hallows", "Ginny Weasley", "Arthur Weasley", "Molly Weasley",
     "Percy Weasley", "Charlie Weasley", "Bill Weasley", "Lucius Malfoy", "Narcissa Malfoy", "Petunia Dursley", "Vernon Dursley",
     "Dudley Dursley", "Luna Lovegood", "Xenophilius Lovegood", "Professor Filius Flitwick", "Professor Pomona Sprout",
 ];
@@ -15,16 +15,35 @@ var letterABC = ["a", "b", "c", "d", "e", "f", "g", "h",
 
 var chosenWord = "";
 var lettersInWord = [];
-var numLetterBlanks = 0;
-var blanksAndSuccesses = [];
+var underscore = document.getElementById("underscore");
+var blanks = "_";
 var wrongLetters = [];
 var correctAnswers = 0;
 var wrongAnswers = 0;
 var guessesLeft = 7;
 var playing = false;
-var userKey = document.getElementById("user-text");
 
-function startGame(startButton){
-    document.getElementById("#start").innerHTML = startButton;
-    console.log(ffff);  
-}
+function startGame() {
+    var chosenWord = hpTerms[Math.floor(Math.random() * hpTerms.length)];
+    lettersInWord = chosenWord.split("");
+    blanks = lettersInWord.length;
+
+    for (chosenWord, i = 0; i < blanks; i++) {
+        underscore = blanks;
+    };
+
+    correctAnswers = document.getElementById("correct");
+
+    // var split = chosenWord.split(underscore);
+    console.log(lettersInWord);
+    // document.getElementById("#").innerHTML = startButton;
+
+};
+//call function
+startGame();
+
+document.onkeyup = function (event) {
+
+    underscore.textContent = event.key;
+   
+};
