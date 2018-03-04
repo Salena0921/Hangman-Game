3b8d69fe93dbf5a3e978cf2f34d9aa55482e3ae1
Karissa McKendall <kmckendall@gmail.com>
2018-03-04 11:02:44 -0500

    finished the css and layout for the start page

---
+++ assets/javascript/startpage.js
@@ -0,0 +1,25 @@
+/*START PAGE*/
+function startGame() {
+    // $(".creature_panel").hide(); 
+    $(".creature_panel").show();
+    $(".heading_welcomePage").hide();
+
+    // $("#pick_your_creature").on("click", function(){
+    //     $(".creature_panel").show(); 
+    //     $(".heading_welcomePage").hide();        
+    // });       
+};
+startGame();
+
+// function housePick(){
+//     $(".houses").on("click", function(){
+//         console.log("hogwarts");
+//         if ($(".houses:first").is("#slytherinpick")){
+//             console.log("slytherin");
+//             $("#hogwartshouse").hide();
+//             $("#slytherin").show(); 
+//             gamePlaySyltherin();        
+//         }
+//     });
+// };
+// housePick();
